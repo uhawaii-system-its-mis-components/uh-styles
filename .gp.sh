5de49d14016944374af730b56c447f55f77b8477
Shaun Blake <sfblake@hawaii.edu>
2016-02-12 13:06:47 -1000

    Remove license and fix imports.

--- .gp.sh
+++ .gp.sh
@@ -3,15 +3,6 @@
 # Modified version of https://github.com/Polymer/tools/blob/master/bin/gp.sh
 # changed to work with Travis CI automated builds
 # Original license follows
-#
-# @license
-# Copyright (c) 2014 The Polymer Project Authors. All rights reserved.
-# This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
-# The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
-# The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
-# Code distributed by Google as part of the polymer project is also
-# subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
-#
 
 # This script pushes a demo-friendly version of your element and its
 # dependencies to gh-pages.
